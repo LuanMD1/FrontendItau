64c0849011323e62ce5c039977c5a0bb1da833c0
LuanMD1 <luan.mdn1@gmail.com>
2025-03-04 19:58:54 -0300

    fix: correções gerais, melhorias de UI e usabilidade

--- front-itau-app/src/Components/ContactButton.jsx
+++ front-itau-app/src/Components/ContactButton.jsx
@@ -43,8 +43,8 @@ export default function ContactButton({vehicleId}) {
                 <ul>
                     <li><strong><FaCar title="Modelo" alt="Icone de Carro"/></strong> {vehicleInfo.model}</li>
                     <li><strong><FaUser title="Nome" alt="Icone de Usuario"/></strong> {vehicleInfo.contact[0]}</li>
-                    <li><strong><FaEnvelope title="Email" alt="Icone de Envelope de email"/></strong> {vehicleInfo.contact[1]}</li>
-                    <li><strong><FaPhone title="Telefone" alt="Icone de Telefone"/></strong> {vehicleInfo.contact[2]}</li>
+                    <li><strong><FaPhone title="Email" alt="Icone de Envelope de email"/></strong> {vehicleInfo.contact[1]}</li>
+                    <li><strong><FaEnvelope title="Telefone" alt="Icone de Telefone"/></strong> {vehicleInfo.contact[2]}</li>
                     <li><p>Visite em<a href="http://icarros.com.br" className="link" target="_blank"> iCarros</a></p></li>
                 </ul>
             ) : (
--- front-itau-app/src/Components/displayContainer.jsx
+++ front-itau-app/src/Components/displayContainer.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./searchBar";
-import { FaHeart } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import ContactButton from "./ContactButton";
 import Footer from "./Footer";
+import ImageCarousel from "./Carousel";
+import FavoriteButton from "./FavoriteButton";
 
 export default function DisplayContainer() {
     const [vehicles, setVehicles] = useState([]);
@@ -25,20 +26,6 @@ export default function DisplayContainer() {
         vehicle.brand.toLowerCase().includes(searchItem.toLowerCase())    
       );
 
-    function toggleFavorite(id) {
-      setVehicles((prevVehicles) =>
-          prevVehicles.map((vehicle) => {
-              if (vehicle.id === id) {
-                  if (!vehicle.isFavorite) {
-                      toast.success("Veículo favoritado!");
-                  }
-                  return { ...vehicle, isFavorite: !vehicle.isFavorite };
-              }
-              return vehicle;
-          })
-      );
-  }
-
     return (
             <section className="card-overlay">
               <div className="content-display">
@@ -49,27 +36,21 @@ export default function DisplayContainer() {
                   <SearchBar onSearch={handleSearch} />
                 </header>
 
-                <div className="vehicles-view-section">
+                <div className="vehicles-view-section mt-2">
                 {filteredVehicles.length > 0 ? (
                   filteredVehicles.map((data) => (
                     <div key={data.id} className="vehicles-card"
                       >
-                        
-                    <a href={data.image}>
-                      <img src={data.image}
-                      className="vehicle-image"
-                       alt={`imagem do veículo ${data.description} cor ${data.color}`}></img>
-                    </a>
+                    <ImageCarousel vehicles={vehicles} vehicleId={data.id}/>
 
                     <div className="toggle-fav-icon" >
-                      <FaHeart title={ "Marcar como favorito "} style={{ color: data.isFavorite ? "red" : "grey", cursor: "pointer"}}
-                      onClick={() => toggleFavorite(data.id)} />
-                    </div>
+                      <h6 className="car-model">{data.model}</h6>
+                      <FavoriteButton vehicleId={data.id}/>
+                    </div> 
 
-                    <h6 className="mt-2">{data.model}</h6>
-                    <h5 className="mt-2">{data.description}</h5>
+                    <h5 className="mt-4">{data.description}</h5>
                       <hr/>
-                    <h6 className="mt-2">{data.price}</h6>
+                    <h6 className="mt-4">{data.price}</h6>
                     <p className="data-info">{data.information.join(", ")}</p>
                       <ContactButton vehicleId={data.id} />
                     </div>
@@ -77,6 +58,7 @@ export default function DisplayContainer() {
                  ) : (
                   <div className="not-found-text">  
                     <p>Nenhum veículo encontrado</p>
+                    <a className="icon" href="/">Voltar a página inicial</a>
                   </div>
                   )}
                 </div>
